3d2626208702059fdc2f8e8d496dbfae70990888
Eduardo Dias Gusmao <edg@outlook.com>
2021-08-07 07:17:14 -0300

    chore(feat): gRPC Service Client

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/codeedu/fc2-grpc/pb"
 	"github.com/codeedu/fc2-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -19,6 +20,7 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
