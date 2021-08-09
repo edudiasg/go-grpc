515de28b222a63cd622d6db720e97c127a8e388b
Eduardo Dias Gusmao <edg@outlook.com>
2021-08-09 07:05:32 -0300

    chore(feat) bidirectional message streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/codeedu/fc2-grpc/pb"
 	"google.golang.org/grpc"
@@ -17,7 +19,15 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+
+	// Unary
+	//AddUser(client)
+
+	// Server Stream
+	//AddUserVerbose(client)
+
+	// Client Stream
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -31,6 +41,76 @@ func AddUser(client pb.UserServiceClient) {
 
 	if err != nil {
 		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Ethan",
+		Email: "ethan@ethan.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	// Loop status
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the msg %v", err)
+		}
+
+		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "u1",
+			Name:  "Ed1",
+			Email: "ed@ed1.com",
+		},
+		&pb.User{
+			Id:    "u2",
+			Name:  "Ed2",
+			Email: "ed@ed2.com",
+		},
+		&pb.User{
+			Id:    "u3",
+			Name:  "Ed3",
+			Email: "ed@ed3.com",
+		},
+		&pb.User{
+			Id:    "u4",
+			Name:  "Ed4",
+			Email: "ed@ed4.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Eror receiving request: %v", err)
 	}
 
 	fmt.Println(res)
